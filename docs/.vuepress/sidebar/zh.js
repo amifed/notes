d7abf6fd0f3a56353f3bd49848fbfb772a644375
jinyang <djy.dingjinyang@gmail.com>
2021-02-09 19:48:41 +0800

    deploy

--- docs/.vuepress/sidebar/zh.js
+++ docs/.vuepress/sidebar/zh.js
@@ -2,8 +2,8 @@
 // if (process.env.NODE_ENV == "production")
 //   process.chdir("/home/runner/work/notes/notes");
 module.exports = {
-  ...require("./algorithm"),
   ...require("./contest"),
+  ...require("./algorithm"),
   ...require("./solution"),
   ...require("./frontend"),
 };
