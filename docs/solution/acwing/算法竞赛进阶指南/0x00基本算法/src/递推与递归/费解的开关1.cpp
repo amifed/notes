ea07aa7335b591f508109aa0c1f0977361e2ca0c
jinyang <djy.dingjinyang@gmail.com>
2021-02-22 12:58:43 +0800

    费解的开关

--- docs/solution/acwing/算法竞赛进阶指南/0x00基本算法/src/递推与递归/费解的开关1.cpp
+++ docs/solution/acwing/算法竞赛进阶指南/0x00基本算法/src/递推与递归/费解的开关1.cpp
@@ -1,27 +1,56 @@
 #include <iostream>
-#include <cstring>
+#include <queue>
+#include <unordered_map>
 using namespace std;
 
-const int N = 7;
 int n;
-char p[N][N], g[N][N];
 const int dx[] = {-1, 0, 1, 0, 0}, dy[] = {0, 1, 0, -1, 0};
+unordered_map<int, int> mp;
 
-int hash() {
-    int ans = 0;
-    for (int i = 0, k = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j, ++k) {
-            if (g[i][j] == '1') ans += 1 << k;
+int turn(int u, int i) {
+    int x = i / 5, y = i % 5;
+    for (int d = 0; d < 5; ++d) {
+        int a = x + dx[d], b = y + dy[d];
+        if (0 <= a && a < 5 && 0 <= b && b < 5) {
+            u ^= 1 << (a * 5 + b);
         }
     }
-    return ans;
+    return u;
+}
+
+void bfs() {
+    int x = (1 << 25) - 1;
+    int cnt = 0;
+    mp[x] = 1;
+    queue<int> q;
+    q.push(x);
+    int ma = 0;
+    while (!q.empty()) {
+        auto p = q.front(); q.pop();
+        if (mp[p] > 6) break;
+        for (int i = 0; i < 25; ++i) {
+            ++cnt;
+            int t = turn(p, i);
+            if (!mp.count(t)) {
+                mp[t] = mp[p] + 1;
+                q.push(t);
+            }
+        }
+        ma = max(ma, (int)q.size());
+    }
 }
 
 int main() {
-    printf("%d", (int)pow(2, 25));
-    // scanf("%d", &n);
-    // while (n--) {
-    //     for (int i = 0; i < 5; ++i) scanf("%s", p[i]);
-    // }
+    bfs();
+    scanf("%d", &n);
+    while (n--) {
+        int x = 0;
+        for (int i = 0; i < 25; ++i) {
+            int a;
+            scanf("%1d", &a);
+            x += (a << i);
+        }
+        printf("%d\n", mp[x] - 1);
+    }
     return 0;
 }
