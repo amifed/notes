b5080a16723901a44157005348929c91aacc3222
jinyang <djy.dingjinyang@gmail.com>
2021-03-03 22:20:36 +0800

    network

--- docs/algorithm/dynamic-programing/状态压缩DP.md
+++ docs/algorithm/dynamic-programing/状态压缩DP.md
@@ -1,1 +1,5 @@
 # 状态压缩 DP
+
+### 相关题目
+
+- [LeetCode 1349. 参加考试的最大学生数](https://leetcode-cn.com/problems/maximum-students-taking-exam/)
--- docs/competition/index.cpp
+++ docs/competition/index.cpp
@@ -1,8 +1,33 @@
 #include <algorithm>
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main() { 
-    cout << (5 & 1) << endl;    
+const int N = 10;
+int f[N][N][2];
+class Solution {
+public:
+    int maxStudents(vector<vector<char>>& seats) {
+        memset(f, 0, sizeof f);
+        int m = seats.size(), n = seats[0].size();
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (seats[i - 1][j - 1] != '.') continue;
+                f[i][j][0] = max({f[i - 1][j - 1][1], f[i - 1][j + 1][1], f[i][j - 1][1]});
+                f[i][j][1] = max({f[i - 1][j - 1][0], f[i - 1][j + 1][0], f[i][j - 1][0]}) + 1;
+            }
+        int ans = 0;
+        for (int i = 1; i <= n; ++i) {
+            ans = max({ans, f[m][i][0], f[m][i][1]});
+        }
+        return ans;
+    }
+};
+
+int main(int argc, char const *argv[]) {
+    vector<vector<char>> s = {{'#', '.', '#', '#', '.', '#'},
+                              {'.', '#', '#', '#', '#', '.'},
+                              {'#', '.', '#', '#', '.', '#'}};
+    cout << Solution().maxStudents(s) << endl;
     return 0;
 }
