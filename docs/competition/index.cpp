d32f9200057221f894e75d95cc60d28639134d94
jinyang <djy.dingjinyang@gmail.com>
2021-03-03 10:38:15 +0800

    network

--- docs/competition/index.cpp
+++ docs/competition/index.cpp
@@ -2,26 +2,7 @@
 #include <iostream>
 using namespace std;
 
-const int N = 307;
-
-int n;
-int s[N];
-int f[N][N];
-
 int main() { 
-    scanf("%d", &n);
-    for (int i = 1; i <= n; ++i) scanf("%d", &s[i]);
-    for (int i = 1; i <= n; ++i) s[i] += s[i - 1];
-    
-    for (int len = 2; len <= n; ++len) {
-        for (int i = 1; i + len - 1 <= n; ++i) {
-            int l = i, r = i + len - 1;
-            f[l][r] = 1e9;
-            for (int k = l; k < r; ++k) {
-                f[l][r] = min(f[l][r], f[l][k] + f[k + 1][r] + s[r] - s[l - 1]);
-            }
-        }
-    }
-    printf("%d", f[1][n]);
+    cout << (5 & 1) << endl;    
     return 0;
 }
--- docs/frontend/interview/js/src/index.js
+++ docs/frontend/interview/js/src/index.js
@@ -1,7 +1,14 @@
-const a = [
-  [0, 0],
-  [-1, 1],
-  [-2, 2],
-];
-
-console.log(Math.max(a.map(([a, b]) => b - a)));
+var arrangeCoins = function(n) {
+  let l = 0,
+    r = n;
+  let ans = 0;
+  while (l < r) {
+    let mid = Math.floor((l + r + 1) / 2);
+    if ((1 + mid) * mid <= 2 * n) {
+      l = mid;
+      ans = Math.max(ans, mid);
+    } else r = mid - 1;
+  }
+  console.log(((1 + l) * l) / 2);
+  return l;
+};
