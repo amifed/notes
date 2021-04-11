4dfb78ecda417c45de2828bd8d18dc7aae1daad1
jinyang <djy.dingjinyang@gmail.com>
2021-04-11 16:23:12 +0800

    lc 234

--- docs/competition/index.cpp
+++ docs/competition/index.cpp
@@ -1,18 +1,44 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
+#define x first
+#define y second
 using namespace std;
 using PII = pair<int, int>;
 
-const int N = 1e4 + 7;
-int n, m;
+const int N = 207, M = 37;
+PII a[N], b[N], c[N];
+int n, q, k;
+
 struct cmp {
     bool operator()(const PII &a, const PII &b) {
-        return a.first != b.first ? a.first < b.first : a.second > b.second;
+        return 1.0 * a.y / a.x < 1.0 * b.y < b.x;
     }
 };
 
 int main() {
-    cout << 6 / -132 << endl;
+    priority_queue<PII, vector<PII>, cmp> pq;
+    scanf("%d%d%d", &n, &q, &k);
+    for (int i = 0; i < n; ++i) {
+        scanf("%d%d", &a[i].x, &a[i].y);
+        pq.emplace(a[i].x, a[i].y);
+    }
+    for (int i = 0; i < q; ++i) {
+        scanf("%d%d", &b[i].x, &b[i].y);
+    }
+    int sum = 0, ep = 0;
+    while (!pq.empty()) {
+        auto p = pq.top(); pq.pop();
+        if (sum + p.x > k) continue;
+        sum += p.x;
+        ep += p.y; 
+    }
+    for (int i = q - 1; i >= 0; --i) {
+        if (sum >= b[i].x) {
+            sum -= b[i].y;
+            break;
+        }
+    }
+    printf("%d %d", sum, ep);
     return 0;
 }
--- docs/frontend/interview/js/src/index.js
+++ docs/frontend/interview/js/src/index.js
@@ -1,9 +1,26 @@
-function PromiseRace(arr) {
-  return new Promise((resolve, reject) => {
-    for (let i = 0; i < arr.length; ++i) {
-      Promise.resolve(arr[i])
-        .then((res) => resolve(res))
-        .catch((err) => reject(err))
-    }
-  })
+const [n, m] = [2, 3]
+const am = [0, 585, 375, 1001]
+const at = [0, 936,317,185, 10001]
+am.sort((a, b) => a - b)
+at.sort((a, b) => a - b)
+console.log(am);
+console.log(at);
+function bs(arr, k) {
+  let l = 0, r = arr.length - 1
+  while (l < r) {
+    let mid = l + r + 1 >> 1
+    if (arr[mid] <= k) l = mid
+    else r = mid - 1
+  }
+  return r
 }
+console.log(bs(am, 587));
+let ans = Number.MIN_SAFE_INTEGER
+for (let d = 1; d <= 1000; ++d) {
+  let im = bs(am, d)
+  let it = bs(at, d)
+  let sm = im + (n - im) * 2
+  let st = it + (m - it) * 2
+  ans = Math.max(st - sm, ans)
+}
+console.log(ans);
