3aeff0cc97406ba92aa43afdc32235e26f1355b9
jinyang <djy.dingjinyang@gmail.com>
2021-04-20 21:36:43 +0800

    hide algo

--- docs/.vuepress/nav/zh.js
+++ docs/.vuepress/nav/zh.js
@@ -1,74 +1,74 @@
 module.exports = [
-  {
-    text: "算法",
-    ariaLabel: "算法",
-    items: [
-      {
-        text: "专题",
-        items: [
-          {
-            text: "基础",
-            link: "/algorithm/basic/",
-          },
-          {
-            text: "数据结构",
-            link: "/algorithm/data-structure/",
-          },
-          {
-            text: "搜索与图论",
-            link: "/algorithm/search-graph/",
-          },
-          {
-            text: "数论",
-            link: "/algorithm/math/",
-          },
-          {
-            text: "动态规划",
-            link: "/algorithm/dynamic-programing/",
-          },
-        ],
-      },
-      {
-        text: "题解",
-        items: [
-          {
-            text: "LeetCode",
-            link: "/solution/leetcode/",
-          },
-          {
-            text: "AcWing",
-            link: "/solution/acwing/",
-          },
-        ],
-      },
-      {
-        text: "其他",
-        items: [
-          {
-            text: "小贴士",
-            link: "/others/tips",
-          },
-          {
-            text: "Latex",
-            link: "/others/latex",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    text: "竞赛",
-    items: [
-      {
-        text: "LeetCode",
-        link: "/competition/leetcode/",
-      },
-      // {
-      //   text: "AtCoder",
-      //   link: "/competition/atcoder/",
-      // },
-    ],
-  },
+  // {
+  //   text: "算法",
+  //   ariaLabel: "算法",
+  //   items: [
+  //     {
+  //       text: "专题",
+  //       items: [
+  //         {
+  //           text: "基础",
+  //           link: "/algorithm/basic/",
+  //         },
+  //         {
+  //           text: "数据结构",
+  //           link: "/algorithm/data-structure/",
+  //         },
+  //         {
+  //           text: "搜索与图论",
+  //           link: "/algorithm/search-graph/",
+  //         },
+  //         {
+  //           text: "数论",
+  //           link: "/algorithm/math/",
+  //         },
+  //         {
+  //           text: "动态规划",
+  //           link: "/algorithm/dynamic-programing/",
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       text: "题解",
+  //       items: [
+  //         {
+  //           text: "LeetCode",
+  //           link: "/solution/leetcode/",
+  //         },
+  //         {
+  //           text: "AcWing",
+  //           link: "/solution/acwing/",
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       text: "其他",
+  //       items: [
+  //         {
+  //           text: "小贴士",
+  //           link: "/others/tips",
+  //         },
+  //         {
+  //           text: "Latex",
+  //           link: "/others/latex",
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
+  // {
+  //   text: "竞赛",
+  //   items: [
+  //     {
+  //       text: "LeetCode",
+  //       link: "/competition/leetcode/",
+  //     },
+  //     // {
+  //     //   text: "AtCoder",
+  //     //   link: "/competition/atcoder/",
+  //     // },
+  //   ],
+  // },
   {
     text: "前端",
     items: [
