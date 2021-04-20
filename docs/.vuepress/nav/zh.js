8218d6741295a440fd1fcf3d0849a26e10a7b08b
jinyang <djy.dingjinyang@gmail.com>
2021-04-20 21:50:50 +0800

    update

--- docs/.vuepress/nav/zh.js
+++ docs/.vuepress/nav/zh.js
@@ -1,125 +1,125 @@
 module.exports = [
-  // {
-  //   text: "算法",
-  //   ariaLabel: "算法",
-  //   items: [
-  //     {
-  //       text: "专题",
-  //       items: [
-  //         {
-  //           text: "基础",
-  //           link: "/algorithm/basic/",
-  //         },
-  //         {
-  //           text: "数据结构",
-  //           link: "/algorithm/data-structure/",
-  //         },
-  //         {
-  //           text: "搜索与图论",
-  //           link: "/algorithm/search-graph/",
-  //         },
-  //         {
-  //           text: "数论",
-  //           link: "/algorithm/math/",
-  //         },
-  //         {
-  //           text: "动态规划",
-  //           link: "/algorithm/dynamic-programing/",
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       text: "题解",
-  //       items: [
-  //         {
-  //           text: "LeetCode",
-  //           link: "/solution/leetcode/",
-  //         },
-  //         {
-  //           text: "AcWing",
-  //           link: "/solution/acwing/",
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       text: "其他",
-  //       items: [
-  //         {
-  //           text: "小贴士",
-  //           link: "/others/tips",
-  //         },
-  //         {
-  //           text: "Latex",
-  //           link: "/others/latex",
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // },
-  // {
-  //   text: "竞赛",
-  //   items: [
-  //     {
-  //       text: "LeetCode",
-  //       link: "/competition/leetcode/",
-  //     },
-  //     // {
-  //     //   text: "AtCoder",
-  //     //   link: "/competition/atcoder/",
-  //     // },
-  //   ],
-  // },
   {
-    text: "前端",
+    text: '算法',
+    ariaLabel: '算法',
     items: [
       {
-        text: "面试指南",
-        items: [
-          { text: "计算机基础", link: "/frontend/interview/cs/os" },
-          { text: "HTML", link: "/frontend/interview/html/html" },
-          { text: "CSS", link: "/frontend/interview/css/css" },
-          { text: "JavaScript", link: "/frontend/interview/js/js" },
-          { text: "Vue", link: "/frontend/interview/vuejs/vue" },
-        ],
-      },
-      {
-        text: "博客文章",
+        text: '专题',
         items: [
           {
-            text: "HTML",
-            link: "",
+            text: '基础',
+            link: '/algorithm/basic/',
           },
           {
-            text: "CSS",
-            link: "",
+            text: '数据结构',
+            link: '/algorithm/data-structure/',
           },
           {
-            text: "JavaScript",
-            link: "",
+            text: '搜索与图论',
+            link: '/algorithm/search-graph/',
           },
           {
-            text: "Vue.js",
-            link: "",
+            text: '数论',
+            link: '/algorithm/math/',
+          },
+          {
+            text: '动态规划',
+            link: '/algorithm/dynamic-programing/',
           },
         ],
       },
       {
-        text: "算法与数据结构",
+        text: '题解',
         items: [
           {
-            text: "算法与数据结构",
-            link: "/frontend/algorithm/",
+            text: 'LeetCode',
+            link: '/solution/leetcode/',
+          },
+          {
+            text: 'AcWing',
+            link: '/solution/acwing/',
+          },
+        ],
+      },
+      {
+        text: '其他',
+        items: [
+          {
+            text: '小贴士',
+            link: '/others/tips',
+          },
+          {
+            text: 'Latex',
+            link: '/others/latex',
           },
         ],
       },
     ],
   },
   {
-    text: "随笔",
-    link: "/personal/",
+    text: '竞赛',
+    items: [
+      {
+        text: 'LeetCode',
+        link: '/competition/leetcode/',
+      },
+      // {
+      //   text: "AtCoder",
+      //   link: "/competition/atcoder/",
+      // },
+    ],
   },
   {
-    text: "关于我",
-    link: "/me/",
+    text: '前端',
+    items: [
+      {
+        text: '面试指南',
+        items: [
+          { text: '计算机基础', link: '/frontend/interview/cs/os' },
+          { text: 'HTML', link: '/frontend/interview/html/html' },
+          { text: 'CSS', link: '/frontend/interview/css/css' },
+          { text: 'JavaScript', link: '/frontend/interview/js/js' },
+          { text: 'Vue', link: '/frontend/interview/vuejs/vue' },
+        ],
+      },
+      {
+        text: '博客文章',
+        items: [
+          {
+            text: 'HTML',
+            link: '',
+          },
+          {
+            text: 'CSS',
+            link: '',
+          },
+          {
+            text: 'JavaScript',
+            link: '',
+          },
+          {
+            text: 'Vue.js',
+            link: '',
+          },
+        ],
+      },
+      {
+        text: '算法与数据结构',
+        items: [
+          {
+            text: '算法与数据结构',
+            link: '/frontend/algorithm/',
+          },
+        ],
+      },
+    ],
   },
-];
+  {
+    text: '随笔',
+    link: '/personal/',
+  },
+  {
+    text: '关于我',
+    link: '/me/',
+  },
+]
