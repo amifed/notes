8a42bcc47e4c021730bfcf5d2787312312720e5c
jinyang <djy.dingjinyang@gmail.com>
2021-05-06 22:03:59 +0800

    es6 commonjs

--- docs/frontend/interview/js/src/infinite_scroll/index.js
+++ docs/frontend/interview/js/src/infinite_scroll/index.js
@@ -48,6 +48,7 @@ show()
 
 window.addEventListener('scroll', function() {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement
+  console.log(scrollTop, scrollHeight, clientHeight)
   if (scrollTop + clientHeight > scrollHeight - 5) {
     ++page
     show()
